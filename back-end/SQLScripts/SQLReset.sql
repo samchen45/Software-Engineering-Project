dec4785fd92eefe66048e2f6ddebe11f3b1378e2
Siyuan Peng <35055252+siyuan-peng@users.noreply.github.com>
2020-10-30 17:20:49 +0800

    SQLReset add test users

--- back-end/SQLScripts/SQLReset.sql
+++ back-end/SQLScripts/SQLReset.sql
@@ -11,12 +11,42 @@ use TCPDB;
 /* TABLES */
 DROP TABLE IF EXISTS `TCPDB`.`users`;
 CREATE TABLE `TCPDB`.`users` (
-  `uid` BIGINT AUTO_INCREMENT,
-  `fullname` VARCHAR(45) NULL,
-  `username` VARCHAR(45) NULL,
+  `uid` BIGINT,
+  `fullname` VARCHAR(20) NULL,
+  `username` VARCHAR(20) NULL,
   `email` VARCHAR(45) NULL,
   `password` VARCHAR(100) NULL,
-  `phonenum` VARCHAR(11) NULL,
-  `type` VARCHAR(10) NULL,
+  `phonenum` BIGINT(11) NULL,
+  `type` VARCHAR(1) NULL,
   PRIMARY KEY (`uid`)
+);
+
+/* GENERATE TEST STUDENT */
+INSERT INTO users (
+  uid, fullname, username, email, password, phonenum, type
+)
+VALUES (
+  10000, 's_fullname', 'student', 'student@test.com', 
+  'pbkdf2:sha256:150000$wdaqj561$6c0c7c628b7b6bd0bb4d9b13510ad7a557ee2dd4d44aa40153b82d173372a03c', 
+  15011111111, 'S'
+);
+
+/* GENERATE TEST TEACHER */
+INSERT INTO users (
+  uid, fullname, username, email, password, phonenum, type
+)
+VALUES (
+  20000, 't_fullname', 'teacher', 'teacher@test.com', 
+  'pbkdf2:sha256:150000$wdaqj561$6c0c7c628b7b6bd0bb4d9b13510ad7a557ee2dd4d44aa40153b82d173372a03c', 
+  15022222222, 'T'
+);
+
+/* GENERATE TEST ADMINISTRATOR */
+INSERT INTO users (
+  uid, fullname, username, email, password, phonenum, type
+)
+VALUES (
+  30000, 'a_fullname', 'admin', 'admin@test.com', 
+  'pbkdf2:sha256:150000$wdaqj561$6c0c7c628b7b6bd0bb4d9b13510ad7a557ee2dd4d44aa40153b82d173372a03c', 
+  11111111111, 'A'
 );
