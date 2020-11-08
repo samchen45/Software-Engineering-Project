77601e40a563596f98ebc6c1fb0513e4e540ac94
wangyue <alex_wy@outlook.com>
2020-11-08 20:59:40 +0800

    Implement file upload

---
+++ back-end/examples/example_load_file/app.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+import os
+from flask import Flask, render_template, send_from_directory, request, jsonify
+
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'upload_file'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER  # 设置文件上传的目标文件夹
+basedir = os.path.abspath(os.path.dirname(__file__))  # 获取当前项目的绝对路径
+ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'xls', 'JPG', 'PNG', 'xlsx', 'gif', 'GIF', 'ppt', 'docx', 'mp4', 'flv'])  # 允许上传的文件后缀
+
+
+# 判断文件是否合法
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+# 具有上传功能的页面
+@app.route('/')
+def main():
+    return render_template('upload.html')
+
+
+@app.route('/api/upload', methods=['POST'], strict_slashes=False)
+def api_upload():
+    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])  # 拼接成合法文件夹地址
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)  # 文件夹不存在就创建
+    f = request.files['myfile']  # 从表单的file字段获取文件，myfile为该表单的name值
+    if f and allowed_file(f.filename):  # 判断是否是允许上传的文件类型
+        fname = f.filename
+        f.save(os.path.join(file_dir, fname))  # 保存文件到upload目录
+
+        return render_template('upload.html', status='OK')
+    else:
+        pass
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
