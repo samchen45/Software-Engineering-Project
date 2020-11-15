49005bf63d943a80719c4628d5f6f59d3e335e13
wangyue <alex_wy@outlook.com>
2020-11-15 20:05:39 +0800

    Implement file download

--- back-end/examples/example_load_file/app.py
+++ back-end/examples/example_load_file/app.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import os
+import time
 from flask import Flask, render_template, send_from_directory, request, jsonify
 
 
@@ -30,12 +31,27 @@ def api_upload():
     f = request.files['myfile']  # 从表单的file字段获取文件，myfile为该表单的name值
     if f and allowed_file(f.filename):  # 判断是否是允许上传的文件类型
         fname = f.filename
-        f.save(os.path.join(file_dir, fname))  # 保存文件到upload目录
 
-        return render_template('upload.html', status='OK')
+        # 根据系统时间重命名文件
+        ext = fname.rsplit('.', 1)[1]  # 获取文件后缀
+        unix_time = int(time.time())
+        new_filename = str(unix_time)+'.'+ext  # 修改了上传的文件名
+        
+        f.save(os.path.join(file_dir, new_filename  ))  # 保存文件到UPLOAD_FOLDER
+        # return render_template('upload.html', status='OK')
+        return jsonify({"errno": 0, "errmsg": "上传成功"})
     else:
-        pass
+        # pass
+        return jsonify({"errno": 1001, "errmsg": "上传失败"})
 
+
+@app.route('/api/download/<filename>', methods=['GET'], strict_slashes=False)
+def download(filename):
+    if request.method == "GET":
+        if os.path.isfile(os.path.join(UPLOAD_FOLDER, filename)):
+            return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+        abort(404)
 
 if __name__ == '__main__':
     app.run(port=5001)
+
