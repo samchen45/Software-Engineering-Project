713dc820b15c58039aa60873bd4f9c447efa8842
Siyuan Peng <35055252+siyuan-peng@users.noreply.github.com>
2020-12-20 15:16:44 +0800

    discussion setup

--- back-end/TCP/api/__init__.py
+++ back-end/TCP/api/__init__.py
@@ -1,3 +1,4 @@
 from TCP.api.accounts import *
 from TCP.api.courses import *
 from TCP.api.homework import *
+from TCP.api.discussion import *
---
+++ back-end/TCP/api/discussion.py
@@ -0,0 +1,3 @@
+from flask import json, request
+import TCP
+import TCP.utils as utils
