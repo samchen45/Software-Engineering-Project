c3bee9af671e56e6b81bd391893477854d3fbe96
OgreSeal <546578510@qq.com>
2021-04-28 19:30:30 +0800

    Add files via upload

--- fore-end/src/routes/Home/index.js
+++ fore-end/src/routes/Home/index.js
@@ -1,17 +1,10 @@
 import React from 'react'
-import {Carousel} from 'antd'
 import './style.css'
-
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <div style={styles.bg} className='home'>
-          <Carousel arrows effect='fade' className='size'>
-            <iframe src="http://dalab.se.sjtu.edu.cn/mse/" height="780px" frameborder="1" scrolling="auto"/>
-          </Carousel>
-        </div>
         <div style={{
           position: 'fixed',
           top: 0,
@@ -22,7 +15,9 @@ class Home extends React.Component {
         }}>
           DALAB实验管理系统
         </div>
-        
+        <div >
+          <iframe src="http://dalab.se.sjtu.edu.cn/mse/" height={window.screen.height * window.devicePixelRatio * 0.70} width="100%" scrolling="auto"/>
+        </div>
       </div>
     )
   }
